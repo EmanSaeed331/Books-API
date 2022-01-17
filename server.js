f4b15daabdf05a1deae1822119da1d7bb4e87ddd
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-17 11:14:47 +0200

    add:server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var cors    = require('cors'); 
+var bodyParser = require('body-parser');
+var storeRoute = require('./router/storeRoute');
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get("/",function(req,res){
+    res.send("Server Started...");
+
+});
+app.use("api/v1/",storeRoute);
+app.listen(port,()=>{
+    console.log('Server start........');
+})
