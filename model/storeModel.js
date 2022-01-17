2e7a394e4d2b07bf247331f3ecdc69b5d1ca57c7
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-17 07:15:54 +0200

    add:storeModel

---
+++ model/storeModel.js
@@ -0,0 +1,9 @@
+exports.Strore = class Store{
+
+    constructor(storeId,storeName,code,address){
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.code = code;
+        this.address = address;
+    }
+}
