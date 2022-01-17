f341d1247013fb98d9b04005427ea20833e18866
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-17 08:07:14 +0200

    add:ddl

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -1,13 +1,27 @@
-create table bms.book1 (
-	book_id serial not null ,
-	book_title varchar(50) not null,
-	book_description varchar(50) , 
-	book_auther varchar(50) not null , 
-	book_publisher varchar(50) not null ,
-	book_pages int null  ,
-	store_code varchar(50) not null , 
-	created_on timestamp not null , 
-	created_by varchar(50) not null , 
-	constraint  book_pkey primary key (book_id)
-	
+-- bms.book definition
+
+-- Drop table
+
+-- DROP TABLE bms.book;
+
+CREATE TABLE bms.book (
+	book_id serial4 NOT NULL,
+	book_title varchar(50) NOT NULL,
+	book_description varchar(50) NULL,
+	book_auther varchar(50) NOT NULL,
+	book_publisher varchar(50) NOT NULL,
+	book_pages int4 NULL,
+	store_code varchar(50) NOT NULL,
+	created_on timestamp NOT NULL,
+	created_by varchar(50) NOT NULL,
+	CONSTRAINT book_pkey PRIMARY KEY (book_id)
+);
+
+CREATE TABLE bms.store (
+	store_id serial4 NOT NULL,
+	store_name varchar(100) NOT NULL,
+	store_code varchar(50) NOT NULL,
+	created_on timestamp NOT NULL,
+	created_by varchar(50) NOT NULL,
+	CONSTRAINT store_pkey PRIMARY KEY (store_id)
 );
