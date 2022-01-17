6e2dd0e999beb810fba5b0abb92fd169c2bdef24
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-17 07:19:10 +0200

    add:bookModel

---
+++ model/bookModel.js
@@ -0,0 +1,13 @@
+exports.Book = class Book{
+
+    constructor(bookId,title,isbn,description,publisher,author,pages){
+     
+        this.bookId =bookId;
+        this.title =title;
+        this.isbn =isbn;
+        this.description =description;
+        this.publisher =publisher;
+        this.author =author;
+        this.pages  = pages;
+    }
+}
