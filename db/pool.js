50b1697ac86b1722875808d82e354f02c4925b04
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-17 09:13:01 +0200

    add:fix:pool

--- db/pool.js
+++ db/pool.js
@@ -4,16 +4,16 @@ const dotenv = require('dotenv');
 
 //Set config of db 
 const db_config = {
-    connectionString:proccess.env.DATABASE_URL,
+    connectionString:process.env.DATABASE_URL,
 
   // all valid client config options are also valid here
   // in addition here are the pool specific configuration parameters:
   // number of milliseconds to wait before timing out when connecting a new client
   // by default this is 0 which means no timeout
-  connectionTimeoutMillis: 300,
+  connectionTimeoutMillie: 300,
   // number of milliseconds a client must sit idle in the pool and not be checked out
   // before it is disconnected from the backend and discarded
-  idleTimeoutMillis?: 200,
+  idleTimeoutMillie?: 200,
   // maximum number of clients the pool should contain
   max?: 20,
   
