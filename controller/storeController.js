e9486cb1148af08bafa203862156d638a0533190
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-17 09:06:21 +0200

    add:get query

--- controller/storeController.js
+++ controller/storeController.js
@@ -1,7 +1,13 @@
+var queries = require('../db/queries');
+var dbConnection = require('../db/connection');
 exports.getStoreList= (req,res)=>{
     try{
-
-        return res.status(200).send(JSON.stringify(values));
+        
+        //get query .
+        var query = queries.queryList.GET_STORE_LIST_QUERY;
+        // await -> make block for code until finishing . 
+        var result  =  await dbConnection.query(query);
+        return res.status(200).send(JSON.stringify(result));
     }
     catch(e){
         return res.status(500).send({err:'Failed to list Store'})
