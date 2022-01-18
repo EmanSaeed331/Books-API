4173ddd25d79a338d29fb2205636ab0a928ba201
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-18 04:35:09 +0200

    add:getBookDetails method

--- controller/bookController.js
+++ controller/bookController.js
@@ -18,3 +18,20 @@ exports.getBookList=async (req,res)=>{
     }
 
 }
+
+exports.getBookDetails=async (req,res)=>{
+    try{
+        var bookId = req.params.id;
+        //get query .
+        var bookDetailsQuery = queries.queryList.GET_BOOK_DETAILS_QUERY;
+        // await -> make block for code until finishing . 
+        var result  =  await dbConnection.dbQuery(bookListQuery,[bookId]);
+        return res.status(200).send(JSON.stringify(result.rows[0]));
+    }
+    catch(err){
+        console.log("Error:"+err)
+        return res.status(500).send({err:'Failed to get book details '})
+
+    }
+
+}
