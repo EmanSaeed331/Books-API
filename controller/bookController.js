aa06e2658020515a13c9c5d221359fa9e1619503
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-18 04:26:58 +0200

    add:getBookList method

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,3 +1,20 @@
 var queries = require('../db/queries');
 var dbConnection = require('../db/connection');
 var util = require('../Util/utility');
+
+exports.getBookList=async (req,res)=>{
+    try{
+        
+        //get query .
+        var bookListQuery = queries.queryList.GET_BOOK_LIST_QUERY;
+        // await -> make block for code until finishing . 
+        var result  =  await dbConnection.dbQuery(bookListQuery);
+        return res.status(200).send(JSON.stringify(result.rows));
+    }
+    catch(err){
+        console.log("Error:"+err)
+        return res.status(500).send({err:'Failed to get book'})
+
+    }
+
+}
